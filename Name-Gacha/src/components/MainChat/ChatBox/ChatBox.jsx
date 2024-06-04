464cd95989e8aa282445555f0b8babfa71dc065e
(max) hyeon bin <uncle.hyeonb@gmail.com>
2024-06-04 15:25:47 +0900

    intall aixos and start to fetch data

--- Name-Gacha/src/components/MainChat/ChatBox/ChatBox.jsx
+++ Name-Gacha/src/components/MainChat/ChatBox/ChatBox.jsx
@@ -1,6 +1,17 @@
 import './ChatBox.css';
+import axios from 'axios';
 
 export default function ChatBox() {
+    const getData = async () => {
+        axios
+            .get('http://localhost:8080/namegacha/api/projects')
+            .then((response) => {
+                console.log('Data fetched:', response.data);
+            })
+            .catch((error) => {
+                console.error('Error fetching data:', error);
+            });
+    };
     return (
         <div className="chat-box-container">
             <div className="chat-content-container">
@@ -11,6 +22,7 @@ export default function ChatBox() {
                         <button>Camel</button>
                         <button>Pascal</button>
                         <button>Snake</button>
+                        <button onClick={() => getData()}>Get Projects</button>
                     </div>
 
                     <div>
