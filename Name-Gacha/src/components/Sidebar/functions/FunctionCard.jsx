d39436f5ffc0e2f781fa03e78cf4811d51c8f726
(max) hyeon bin <uncle.hyeonb@gmail.com>
2024-06-16 23:50:56 +0900

    implemente function done

--- Name-Gacha/src/components/Sidebar/functions/FunctionCard.jsx
+++ Name-Gacha/src/components/Sidebar/functions/FunctionCard.jsx
@@ -2,13 +2,30 @@
 
 import { TbFunction } from 'react-icons/tb';
 import './FnCard.css';
-export default function FunctionCard({ functions }) {
+import * as fnAPI from '../../../utils/api/aws/functionRoutes';
+import { useMutation } from 'react-query';
+
+export default function FunctionCard({ functions, pageId }) {
+    const { mutate: addFunction } = useMutation({
+        mutationFn: ({ functionName, pageId }) => {
+            return fnAPI.createFunction(functionName, pageId);
+        },
+    });
+
     return (
         <div className="fns-container">
             <div className="fn-name">
                 <TbFunction className="icon" size="1.2rem" />
                 function container
-                <i className="icon-basic-elaboration-message-plus"></i>
+                <i
+                    className="icon-basic-elaboration-message-plus"
+                    onClick={() =>
+                        addFunction({
+                            functionName: 'add function test',
+                            pageId: pageId,
+                        })
+                    }
+                ></i>
             </div>
             <div style={{ paddingLeft: '30%' }}>
                 <ul>
