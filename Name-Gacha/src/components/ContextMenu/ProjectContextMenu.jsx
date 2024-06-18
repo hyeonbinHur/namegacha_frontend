0639e071d3a5d0fa62db26a02410346d687fc1be
(max) hyeon bin <uncle.hyeonb@gmail.com>
2024-06-18 14:56:17 +0900

    delete project using context menu done

--- Name-Gacha/src/components/ContextMenu/ProjectContextMenu.jsx
+++ Name-Gacha/src/components/ContextMenu/ProjectContextMenu.jsx
@@ -1,6 +1,8 @@
 /* eslint-disable react/prop-types */
 import { useDispatch } from 'react-redux';
 import { addChild, editItSelf } from '../../store/contextMenuSlice';
+import * as projectAPI from '../../utils/api/aws/projectRoutes';
+import { useMutation, useQueryClient } from 'react-query';
 
 export default function ProjectContextMenu({ target }) {
     const dispatch = useDispatch();
@@ -15,12 +17,24 @@ export default function ProjectContextMenu({ target }) {
         );
         e.stopPropagation();
     };
+    const queryClient = useQueryClient();
+
+    const { mutate: deleteProject } = useMutation({
+        mutationFn: ({ projectId }) => {
+            return projectAPI.deleteProject(projectId);
+        },
+        onSuccess: () => {
+            queryClient.invalidateQueries('getCertainProjects');
+        },
+    });
 
     return (
         <div>
             <button onClick={(e) => startRename(e)}>Rename</button>
             <div onClick={(e) => startAddChild(e)}>New Page</div>
-            <div>Delete Project</div>
+            <div onClick={() => deleteProject({ projectId: target.id })}>
+                Delete Project
+            </div>
             <div>Share</div>
         </div>
     );
