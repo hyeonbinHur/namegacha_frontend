8aa09a8dfc97c3dd4d1ce8ff9ab9e104e8b987e9
(max) hyeon bin <uncle.hyeonb@gmail.com>
2024-06-24 22:13:33 +0900

    refactorize identifier modal

--- Name-Gacha/src/components/Modal/IdentifierModal.jsx
+++ Name-Gacha/src/components/Modal/IdentifierModal.jsx
@@ -1,20 +1,16 @@
 /* eslint-disable react/prop-types */
-import { forwardRef, useState } from 'react';
+import { forwardRef, useImperativeHandle, useState, useRef } from 'react';
 import { useQuery, useMutation } from '@tanstack/react-query';
 import { getCertainProjects } from '../../utils/api/aws/projectRoutes';
 import { createVariable } from '../../utils/api/aws/variableRoutes';
 import { createFunction } from '../../utils/api/aws/functionRoutes';
+import { createPortal } from 'react-dom';
 
 const IdentifierModal = forwardRef(function IdentifierModal(
     { user, type, newIdentifier },
     ref
 ) {
-    // 유저를 프롭으로
-    // 여기서 프로젝트를 query key를 이용해서 사용, 캐시에 없을 경우, 여기서 동작
-    // 어떤 페이지가 눌렸는가를 확인,
-    // 페이지 정보 가져오는 api 사용
-    // add
-
+    const modal = useRef(null);
     const [selectedProject, setSelectedProject] = useState(null);
     const [selectedPage, setSelectedPage] = useState(null);
     const findItem = (type, id) => {
@@ -65,76 +61,90 @@ const IdentifierModal = forwardRef(function IdentifierModal(
             });
         }
     };
-
-    return (
+    useImperativeHandle(ref, () => {
+        return {
+            open: () => {
+                modal.current.showModal();
+            },
+            close: () => {
+                modal.current.close();
+            },
+        };
+    });
+    return createPortal(
         <div>
-            <button> close </button>
-            <div className="project-contaier">
-                <ul>
-                    {/* project card */}
-                    {projects.map((project) => (
-                        <li key={project}>
-                            <ModalCard
-                                onClick={() =>
-                                    findItem('project', project.projectId)
-                                }
-                                name={project.projectName}
-                                id={project.ProjectId}
-                                type={'project'}
-                            />
-                        </li>
-                    ))}
-                </ul>
-            </div>
-            <div className="page-container">
-                <ul>
-                    {/* page card */}
-                    {selectedProject.pages.map((page) => (
-                        <li key={page}>
-                            <ModalCard
-                                onClick={() => findItem('project', page.pageId)}
-                                name={page.pageName}
-                                id={page.pageId}
-                                type={'page'}
-                            />
-                        </li>
-                    ))}
-                </ul>
-            </div>
-            <div className="identifier-container">
-                {type === 'variable' ? (
-                    <div>
-                        {/* variable card */}
-                        {selectedPage.variables.map((variable) => (
-                            <li key={variable}>
+            <dialog ref={modal}>
+                <button> close </button>
+                <div className="project-contaier">
+                    <ul>
+                        {/* project card */}
+                        {projects.map((project) => (
+                            <li key={project}>
                                 <ModalCard
-                                    name={variable.variableName}
-                                    id={variable.variableId}
+                                    onClick={() =>
+                                        findItem('project', project.projectId)
+                                    }
+                                    name={project.projectName}
+                                    id={project.ProjectId}
+                                    type={'project'}
                                 />
                             </li>
                         ))}
-                    </div>
-                ) : (
-                    <div>
-                        {/* function card */}
-                        {selectedPage.functions.map((fn) => (
-                            <li key={fn}>
-                                <ModalCard
-                                    name={fn.functionName}
-                                    id={fn.functionId}
-                                />
-                            </li>
-                        ))}
-                    </div>
-                )}
-                <div className="newitem-container">
-                    <div>{newIdentifier.name}</div>
-                    <div>{newIdentifier.exp}</div>
+                    </ul>
                 </div>
-            </div>
-            <button onClick={() => startAddIdentifier()}> save </button>
-            <button> cancel </button>
-        </div>
+                <div className="page-container">
+                    <ul>
+                        {/* page card */}
+                        {selectedProject.pages.map((page) => (
+                            <li key={page}>
+                                <ModalCard
+                                    onClick={() =>
+                                        findItem('project', page.pageId)
+                                    }
+                                    name={page.pageName}
+                                    id={page.pageId}
+                                    type={'page'}
+                                />
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                <div className="identifier-container">
+                    {type === 'variable' ? (
+                        <div>
+                            {/* variable card */}
+                            {selectedPage.variables.map((variable) => (
+                                <li key={variable}>
+                                    <ModalCard
+                                        name={variable.variableName}
+                                        id={variable.variableId}
+                                    />
+                                </li>
+                            ))}
+                        </div>
+                    ) : (
+                        <div>
+                            {/* function card */}
+                            {selectedPage.functions.map((fn) => (
+                                <li key={fn}>
+                                    <ModalCard
+                                        name={fn.functionName}
+                                        id={fn.functionId}
+                                    />
+                                </li>
+                            ))}
+                        </div>
+                    )}
+                    <div className="newitem-container">
+                        <div>{newIdentifier.name}</div>
+                        <div>{newIdentifier.exp}</div>
+                    </div>
+                </div>
+                <button onClick={() => startAddIdentifier()}> save </button>
+                <button> cancel </button>
+            </dialog>
+        </div>,
+        document.getElementById('modal')
     );
 });
 const ModalCard = () => {
