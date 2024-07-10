7990f7c84e1f7600ab5600aee9688351c3701452
(max) hyeon bin <uncle.hyeonb@gmail.com>
2024-07-10 23:18:23 +0900

    Delete test pages

--- Name-Gacha/src/pages/AuthPage.jsx
+++
@@ -1,9 +0,0 @@
-// import LoginForm from '../components/Auth/LoginForm';
-
-// export default function AuthPage() {
-//     return (
-//         <div>
-//             <LoginForm />
-//         </div>
-//     );
-// }
