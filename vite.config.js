8b99dea42e780f5a95dbdb7656a6f41c6e7ca926
Hur Hyeon Bin (Max) <160996936+hyeonbinHur@users.noreply.github.com>
2024-12-29 18:03:29 +0900

    testing

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,14 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    plugins: [react()],
-    base: 'namegacha/',
-    test: {
-        globals: true,
-        environment: 'jsdom',
-        setupFiles: './vitest.setup.js',
-        coverage: {
-            include: ['src/__test__/*.{js,jsx}'],
-            exclude: ['src/mocks/*'],
-        },
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./vitest.setup.js",
+    coverage: {
+      include: ["src/__test__/*.{js,jsx}"],
+      exclude: ["src/mocks/*"],
     },
+  },
 });
